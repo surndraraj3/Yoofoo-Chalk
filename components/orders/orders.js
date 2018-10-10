20478c32d52a23fdec07d64564705316fb8cd787
surndraraj3 <surendraraj3@gmail.com>
2018-10-10 17:53:04 +0530

    Orders Screen

--- components/orders/orders.js
+++ components/orders/orders.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Icon,
+  Button,
+  Header,
+  Body,
+  Left,
+  Right,
+  Title
+} from "native-base";
+import commonStyles from "../styles/styles";
 
 export default class Orders extends React.Component {
-    render() {
-        return(
-            <Text>Welcome to Order Page</Text>
-        );
-    }
+  render() {
+    return (
+      <Container>
+          <View style={{ padding: 10 }} />
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("Customer")}
+            >
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Profile</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="home" />
+            </Button>
+            <Button transparent>
+              <Icon name="more" />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <Form>
+            <Label style={commonStyles.labelPos}>Welcome To Order Page</Label>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
 }
