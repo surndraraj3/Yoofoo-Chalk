5913beef49555595e27dbfd6ba9bf19f77ed307b
surndraraj3 <surendraraj3@gmail.com>
2018-10-22 12:10:31 +0530

    CC-97 Add New Customer

--- components/common/url_config.js
+++ components/common/url_config.js
@@ -1,7 +1,9 @@
 //Login URL POST
 // export const baseURL = 'http://chalkcoutureapiloginservice-dev.us-west-1.elasticbeanstalk.com/api/';
-export const baseURL = 'http://ccmobileloginservice-dev.us-west-1.elasticbeanstalk.com/api/';
+export const baseURL = 'http://loginservice-dev.us-west-1.elasticbeanstalk.com/api/';
 //Customer Profile URL GET
 export const custPrflURL = 'http://chalkcoutureapicustomerservices-dev.us-west-1.elasticbeanstalk.com/api/Customer/';
 // Order URL GET by customer id
 export const customerOrderURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/';
+//Add Customer Screen
+export const add_customerURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/CreateCustomer';
--- components/customers/add_customer.js
+++ components/customers/add_customer.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, Text, Switch, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Switch,
+  ScrollView,
+  ActivityIndicator
+} from "react-native";
 import {
   Container,
   Content,
@@ -18,20 +24,111 @@ import {
   CardItem,
   Card
 } from "native-base";
+import { add_customerURL } from "../common/url_config";
 import commonStyles from "../styles/styles";
 
 export default class AddCutsomer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      valBillingAddress: false
+      valBillingAddress: false,
+      customerEmail: "",
+      customerFirstName: "",
+      customerLastName: "",
+      customerPhoneNUmber: "",
+      validateEmail: "",
+      errorEmail: false,
+      loadSpinner: false,
+      loadFormMessage: false,
+      customerData: ""
     };
   }
+  //Display Billing Address form based on condition
   onBillingAddressChange = itm => {
     // console.log("itm", itm);
     if (itm) this.setState({ valBillingAddress: true });
     else this.setState({ valBillingAddress: false });
   };
+  //Get Email from textbox
+  onEmailChange = txtEmail => {
+    this.setState({ customerEmail: txtEmail });
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (reg.test(txtEmail) === false) {
+      // console.log("Email is Not Correct");
+      this.setState({
+        validateEmail: "Invalid Email",
+        errorEmail: true
+      });
+      return false;
+    } else {
+      this.setState({ validateEmail: "Email is Correct", errorEmail: false });
+      // console.log("Email is Correct");
+    }
+  };
+  // Get First Name from textbox
+  onFirstNameChange = txtFirstName => {
+    this.setState({ customerFirstName: txtFirstName });
+  };
+  //Get Last name from textbox
+  onLastNameChange = txtLastName => {
+    this.setState({ customerLastName: txtLastName });
+  };
+  //Get Phone Number from textbox
+  onPhoneNumberChange = txtPhoneNumber => {
+    this.setState({ customerPhoneNUmber: txtPhoneNumber });
+  };
+
+  //Save Customer Form
+  saveCustomerDetails = (email, firstNm, lastNm, phoneNum) => {
+    console.log("Cust Details", email, firstNm, lastNm, phoneNum);
+    this.setState({ loadSpinner: true });
+    fetch(`${add_customerURL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        Email: email,
+        FirstName: firstNm,
+        LastName: lastNm,
+        DistributorID: "14711",
+        Phone: phoneNum
+      })
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        // console.log(responseJson);
+        this.setState({
+          customerData: responseJson
+        });
+        console.log("Data", this.state.customerData);
+        this.setState({ loadFormMessage: true, loadSpinner: false });
+        if (this.state.customerData.Result !== "Failure") {
+          console.log('Success');
+          this.setState({
+            customerEmail: "",
+            customerFirstName: "",
+            customerLastName: "",
+            customerPhoneNUmber: ""
+          });
+        } else {
+          console.log('Fail');
+          this.setState({
+            customerEmail: email,
+            customerFirstName: firstNm,
+            customerLastName: lastNm,
+            customerPhoneNUmber: phoneNum
+          });
+        }
+        setTimeout(() => {
+          this.setState({ loadFormMessage: false });
+        }, 3000);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   render() {
     return (
       <Container>
@@ -63,25 +160,35 @@ export default class AddCutsomer extends React.Component {
               <CardItem>
                 <Item stackedLabel>
                   <Label>Email</Label>
-                  <Input />
+                  <Input onChangeText={this.onEmailChange} value={this.state.customerEmail} />
                 </Item>
               </CardItem>
+              {this.state.errorEmail && (
+                <Label style={commonStyles.errorMsg}>
+                  {this.state.validateEmail}
+                </Label>
+              )}
               <CardItem>
                 <Item stackedLabel>
                   <Label>First Name</Label>
-                  <Input />
+                  <Input onChangeText={this.onFirstNameChange} value={this.state.customerFirstName} />
                 </Item>
               </CardItem>
               <CardItem>
                 <Item stackedLabel>
                   <Label>Last Name</Label>
-                  <Input />
+                  <Input onChangeText={this.onLastNameChange} value={this.state.customerLastName} />
                 </Item>
               </CardItem>
               <CardItem>
                 <Item stackedLabel>
                   <Label>Phone Number</Label>
-                  <Input />
+                  <Input
+                    onChangeText={this.onPhoneNumberChange}
+                    keyboardType="numeric"
+                    maxLength={10}   
+                    value={this.state.customerPhoneNUmber}                
+                  />
                 </Item>
               </CardItem>
             </Card>
@@ -130,9 +237,29 @@ export default class AddCutsomer extends React.Component {
                 </View>
               )}
             </Card>
+            {this.state.loadSpinner && (
+              <ActivityIndicator size="large" color="#0000ff" />
+            )}
+            {this.state.loadFormMessage && (
+              <Label style={commonStyles.errorMsg}>
+                {/* Incorrect credentials were supplied */}
+                {/* {this.state.customerData.Message} */}
+                {this.state.customerData.Result === "Success"
+                  ? this.state.customerData.Result
+                  : this.state.customerData.Message}
+              </Label>
+            )}
             <View style={commonStyles.buttonPos}>
               <Text
                 style={{ color: "#42f4f1", fontSize: 20, fontWeight: "bold" }}
+                onPress={() => {
+                  this.saveCustomerDetails(
+                    this.state.customerEmail,
+                    this.state.customerFirstName,
+                    this.state.customerLastName,
+                    this.state.customerPhoneNUmber
+                  );
+                }}
               >
                 Save
               </Text>
