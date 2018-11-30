3e7a039f49ea3eef4a2a69f23542c104c0686006
surendra nadh <surendranadh@surendras-Mac-mini.local>
2018-11-30 17:42:37 +0530

    Prod Build

--- components/common/url_config.js
+++ components/common/url_config.js
@@ -1,27 +1,57 @@
+// ----------------Production Urls----------------------------
 //Login URL POST
 // export const baseURL = 'http://chalkcoutureapiloginservice-dev.us-west-1.elasticbeanstalk.com/api/';
-export const baseURL = 'http://loginservice-dev.us-west-1.elasticbeanstalk.com/api/';
+export const baseURL = 'http://chalkcoutureapiloginservice-prod.us-west-1.elasticbeanstalk.com/api/';
 //Customer Profile URL GET
-export const custPrflURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/GetExigoCustomer/'  //'http://chalkcoutureapicustomerservices-dev.us-west-1.elasticbeanstalk.com/api/Customer/';
+export const custPrflURL = 'http://chalkcoutureapicustomerservices-prod.us-west-1.elasticbeanstalk.com/api/customer/GetExigoCustomer/' //'http://chalkcoutureapicustomerservices-dev.us-west-1.elasticbeanstalk.com/api/Customer/';
 // Order URL GET by customer id
-export const customerOrderURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/';
+export const customerOrderURL = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/orders/';
 //Add Customer Screen
-export const add_customerURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/CreateCustomer';
+export const add_customerURL = 'http://chalkcoutureapicustomerservices-prod.us-west-1.elasticbeanstalk.com/api/customer/CreateCustomer';
 //Get List of CUstomers URL
-export const getCustomerListURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/GetChalkCustomers/';
+export const getCustomerListURL = 'http://chalkcoutureapicustomerservices-prod.us-west-1.elasticbeanstalk.com/api/customer/GetChalkCustomers/';
 //Get List of Orders URL
-export const getOrdersListURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/OrdersByDesignerID/';
+export const getOrdersListURL = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/orders/OrdersByDesignerID/';
 //Get Inventory List 
-export const getInventoryListURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/InventoryByDesignerID/';
+export const getInventoryListURL = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/Orders/InventoryByDesignerID/';
 //Add Orders 
-export const addOrdersUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/AddOrder/';
+export const addOrdersUrl = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/Orders/AddOrder/';
 //Get Invoice Details Based on Order Id
-export const getInvoiceDetailsByOrderIdUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/GetINvoice/'
+export const getInvoiceDetailsByOrderIdUrl = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/Orders/GetINvoice/'
 
 //Post caluclating orders
-export const calculateAddOrdersUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/CalculateOrder/'
+export const calculateAddOrdersUrl = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/Orders/CalculateOrder/'
 
 //Zip Code Validation Url
-export const zipCodeUrl = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/ValidateZip/';
+export const zipCodeUrl = 'http://chalkcoutureapicustomerservices-prod.us-west-1.elasticbeanstalk.com/api/customer/ValidateZip/';
 //POST Cash Payment
-export const postCashPaymentUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/PostCashPayment';
+export const postCashPaymentUrl = 'http://chalkcoutureapiorderservice-prod.us-west-1.elasticbeanstalk.com/api/orders/PostCashPayment';
+
+
+//------------------Development URLS--------------------
+// // export const baseURL = 'http://chalkcoutureapiloginservice-dev.us-west-1.elasticbeanstalk.com/api/';
+// export const baseURL = 'http://loginservice-dev.us-west-1.elasticbeanstalk.com/api/';
+// //Customer Profile URL GET
+// export const custPrflURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/GetExigoCustomer/'  //'http://chalkcoutureapicustomerservices-dev.us-west-1.elasticbeanstalk.com/api/Customer/';
+// // Order URL GET by customer id
+// export const customerOrderURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/';
+// //Add Customer Screen
+// export const add_customerURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/CreateCustomer';
+// //Get List of CUstomers URL
+// export const getCustomerListURL = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/GetChalkCustomers/';
+// //Get List of Orders URL
+// export const getOrdersListURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/OrdersByDesignerID/';
+// //Get Inventory List 
+// export const getInventoryListURL = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/InventoryByDesignerID/';
+// //Add Orders 
+// export const addOrdersUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/AddOrder/';
+// //Get Invoice Details Based on Order Id
+// export const getInvoiceDetailsByOrderIdUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/GetINvoice/'
+
+// //Post caluclating orders
+// export const calculateAddOrdersUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/Orders/CalculateOrder/'
+
+// //Zip Code Validation Url
+// export const zipCodeUrl = 'http://customerservices-dev.us-west-1.elasticbeanstalk.com/api/customer/ValidateZip/';
+// //POST Cash Payment
+// export const postCashPaymentUrl = 'http://ccapiorderservice-dev.us-west-1.elasticbeanstalk.com/api/orders/PostCashPayment';
